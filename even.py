b81fc91e556178a2ed5cdd2957bfa8600d62be8d
Giridharan <52557361+giridharan25@users.noreply.github.com>
2019-07-06 14:30:54 +0530

    Update even.py

--- even.py
+++ even.py
@@ -4,8 +4,8 @@ if(z<=0):
     print("invalid")
 else:
     if(result==0):
-        print("Even")
-    elif(result==1):
-        print("Odd")
+            print("Even")
+        elif(result==1):
+            print("Odd")
     else:
         print("invalid")
