ec77af7032e70a734626c5ef9c833389351fb44e
Giridharan <52557361+giridharan25@users.noreply.github.com>
2019-07-11 00:18:27 +0530

    Update Primebw2.py

--- Primebw2.py
+++ Primebw2.py
@@ -1,7 +1,8 @@
 a,s=map(int,input().split())
-for i in range(a+1,s):
+for i in range(a,s+1):
+ if(i>1):
    for j in range(2,i):
      if (i%j==0):
         break
-     else:        
-        print(i,end=" ")
+ else:        
+    print(i,end=" ")
