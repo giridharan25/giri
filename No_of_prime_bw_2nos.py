48178abd27d20571782c91be8b2de0bd227d27d2
Giridharan <52557361+giridharan25@users.noreply.github.com>
2019-07-13 01:39:47 +0530

    Update No_of_prime_bw_2nos.py

--- No_of_prime_bw_2nos.py
+++ No_of_prime_bw_2nos.py
@@ -1,11 +1,9 @@
-a,s=map(int,input().split())
-f=0
-for i in range(a,s):
-   for j in range(2,i):
-     if (i%j==0):
-        break
-     else:
-        f=f+1
- print(f)    
-
-     
+a,d=map(int,input().split())
+f=[]
+for b in range (a,d+1):
+    for c in range(2,b):
+        if (b%c==0):
+            break
+        else:
+            f.append(b)
+print(len(f))
