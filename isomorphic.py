389963ae4502e60b5ae3ebadc05ed22d1c66a44d
Giridharan <52557361+giridharan25@users.noreply.github.com>
2019-07-19 13:52:58 +0530

    Update isomorphic.py

--- isomorphic.py
+++ isomorphic.py
@@ -1,7 +1,10 @@
-x,y=map(str,input().split())
-a=len(x)
-b=len(y)
-if(a==b):
-    print("yes")
+x,y=list(input().split())
+if(len(x)==len(y)):
+    for i in range(0,len(x)):
+        if(y[i]==y[i+1] and x[i]==x[i+1]):
+            print("yes")
+            break
+        else:
+            print("no")
 else:
     print("no")
