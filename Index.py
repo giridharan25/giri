3dbe86dd9429d0487edf8c91eef774c695207db1
Giridharan <52557361+giridharan25@users.noreply.github.com>
2019-07-21 18:12:22 +0530

    Create Index.py

---
+++ Index.py
@@ -0,0 +1,10 @@
+a=int(input())
+b=list(map(int,input().split(None,a)[:a]))
+a=[]
+for i in range(len(b)):
+    if b[i]==i:       
+        a.append(b[i])
+if len(a)==0:
+    print(-1)
+else:
+    print(" ".join(map(str,a)))
