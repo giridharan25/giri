c4e81fa15519a76589955185232f5470be4824ac
Giridharan <52557361+giridharan25@users.noreply.github.com>
2019-08-09 22:49:38 +0530

    Create AZ.py

---
+++ AZ.py
@@ -0,0 +1,14 @@
+x,y=map(str,input().split())
+sum1=0
+sum2=0
+for i in x:
+  if(i not in y):
+     sum1=sum1+ord(i)
+for j in y:
+  if(j not in x):
+     sum2=sum2+ord(j)
+o=sum2-sum1
+p=o
+if(o>96):
+    p=o-96
+print(p)
