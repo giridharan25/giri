c6b9ee788d6cfef88dd0d636dbba0a79974d5f85
Giridharan <52557361+giridharan25@users.noreply.github.com>
2019-08-10 20:20:13 +0530

    Create ijk.py

---
+++ ijk.py
@@ -0,0 +1,8 @@
+n=int(input())
+l=list(map(int,input().split()))
+for i in range(0,n):
+  for j in range(1,n):
+    for k in range(2,n):
+       if(l[i]<l[j]<l[k]):
+           if(l[i]+l[j]==l[k]):
+               print(l[i],l[j],l[k])
