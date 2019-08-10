f557b2301c8c45297ce9ee30df23a8b9ad2b2000
Giridharan <52557361+giridharan25@users.noreply.github.com>
2019-08-10 21:49:50 +0530

    Update Zero.py

--- Zero.py
+++ Zero.py
@@ -1,10 +1,24 @@
 n=int(input())
 l=list(map(int,input().split()))
 s=[]
+f=[]
+g=[]
 for i in l:
   for j in l[1:n]:
-     if(i+j==0):
-        
+     if((i+j==0)):
         s.append(i)
         s.append(j)
-print(s[0],s[1])
+     elif(i+j==1):
+        f.append(i)
+        f.append(j)
+     elif(i+j==-1):
+        g.append(i)
+        g.append(j)
+a=len(s)
+b=len(f)
+if(a!=0):
+    print(s[0],s[1])
+elif(a==0):
+    print(f[0],f[1])
+else:
+     print(g[0],g[1])
