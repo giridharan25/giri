41cbe9bdb3af54963f6bc0d761a37458305dd43c
Giridharan <52557361+giridharan25@users.noreply.github.com>
2019-08-30 22:10:29 +0530

    Create Catalan.py

---
+++ Catalan.py
@@ -0,0 +1,10 @@
+def catalan_number(num):
+    if num <=1:
+        return 1 
+    res_num = 0
+    for i in range(num):
+        res_num += catalan_number(i) * catalan_number(num-i-1)
+    return res_num
+z=int(input())
+for n in range(z+1):
+    print(catalan_number(n),end=" ")
